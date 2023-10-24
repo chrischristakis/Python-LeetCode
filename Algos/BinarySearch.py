86eb68dfa7d38eb72abd3e415ac47a260a0c86b6
chrischristakis <chriscchristakis@gmail.com>
2023-10-23 22:26:31 -0400

    binary search and sudoku

---
+++ Algos/BinarySearch.py
@@ -0,0 +1,43 @@
+def iter_bin_search(arr, target):
+    left = 0
+    right = len(arr) - 1
+    while left <= right:
+        middle = left + (right - left) // 2
+        val = arr[middle]
+
+        if val > target:
+            right = middle - 1
+        elif val < target:
+            left = middle + 1
+        else:
+            return True
+
+    return False
+
+
+def recursive_bin_search(arr, target):
+    def inner(left, right):
+        if left > right:
+            return False
+
+        middle = left + (right - left) // 2
+        val = arr[middle]
+
+        if val > target:
+            return inner(left, middle - 1)
+        elif val < target:
+            return inner(middle + 1, right)
+        else:
+            return True
+
+    return inner(0, len(arr) - 1)
+
+
+print(recursive_bin_search([3, 5, 8, 9, 11, 12, 13, 15, 17, 18, 19, 20, 22, 23, 25, 28, 29,
+                  30, 37, 38, 40, 41, 42, 44, 45, 50, 51, 52, 53, 56, 57, 58, 59,
+                  60, 66, 70, 76, 77, 81, 82, 83, 84, 85, 89, 90, 91, 92, 96, 98,
+                  99, 100, 110, 130, 131, 135, 190, 201, 215, 219, 256, 257, 300],
+
+                  13))
+
+
--- ValidSudoku.py
+++ ValidSudoku.py
@@ -31,7 +31,29 @@ def valid_sudoku(board):
     return True
 
 
-print(valid_sudoku(
+def valid_sudoku2(board):
+    row_list = {}
+    col_list = {}
+    chunk_list = {}
+    for row in range(0, 9):
+        for col in range(0, 9):
+            if board[row][col] == '.':
+                continue
+
+            val = int(board[row][col]) - 1  # subtract 1 since numbers are 1-9, we want 0-8 for indexing
+
+            # We have 9 chunks indexed [0,8], this formula calculates which chunk index (row,col) is in
+            chunk_index = (3 * (row // 3)) + (col // 3)
+
+            if (row,val) in row_list or (col, val) in col_list or (chunk_index, val) in chunk_list:
+                return False
+
+            row_list[(row,val)] = col_list[(col,val)] = chunk_list[(chunk_index,val)] = 1
+
+    return True
+
+
+print(valid_sudoku2(
         [["5","3",".",".","7",".",".",".","."]
         ,["6",".",".","1","9","5",".",".","."]
         ,[".","9","8",".",".",".",".","6","."]
@@ -42,7 +64,7 @@ print(valid_sudoku(
         ,[".",".",".","4","1","9",".",".","5"]
         ,[".",".",".",".","8",".",".","7","9"]]
 ))
-print(valid_sudoku(
+print(valid_sudoku2(
         [["8","3",".",".","7",".",".",".","."]
         ,["6",".",".","1","9","5",".",".","."]
         ,[".","9","8",".",".",".",".","6","."]
@@ -53,3 +75,4 @@ print(valid_sudoku(
         ,[".",".",".","4","1","9",".",".","5"]
         ,[".",".",".",".","8",".",".","7","9"]]
 ))
+
